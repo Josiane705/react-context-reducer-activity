fbebc0587a4b2326846de2ac0164804a8e725d1d
Josiane705 <j.mukeshima@alustudent.com>
2026-09-25 12:17:34 +0200

    Add Navbar with theme toggle

---
+++ react-state-management/src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { useTheme } from "../context/ThemeContext";
+import styles from "./Navbar.module.css";
+import { DARK_THEME, LIGHT_THEME } from "../constants/theme";
+
+const Navbar = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <nav className={`${styles.navbar} ${theme === LIGHT_THEME ? styles.light : styles.dark}`}>
+      <span className={styles.brand}>React App</span>
+      <button className={styles.toggleButton} onClick={toggleTheme}>
+        Switch to {theme === LIGHT_THEME ? DARK_THEME : LIGHT_THEME} Mode
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
